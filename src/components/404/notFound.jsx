60c77de7dc6b5e0aacaf4ffb97387b3dd5f402f3
cswebdev <csnider91@live.com>
2024-06-18 14:17:07 -0400

    installed react-router and react-bootstrap

---
+++ src/components/404/notFound.jsx
@@ -0,0 +1,7 @@
+import "./notFound.css";
+
+const notFound = () => {
+   <h1>not Found</h1>;
+};
+
+export default notFound;
---
+++ src/components/UnderConstruction/UnderConstruction.jsx
@@ -0,0 +1,14 @@
+import "./underConstruction.css";
+
+const UnderConstruction = () => {
+   return (
+      <>
+         <div className="construction-container">
+            <h1>This Page is Under Construction</h1>
+            <h2>Please Check Back Later</h2>
+         </div>
+      </>
+   );
+};
+
+export default UnderConstruction;
