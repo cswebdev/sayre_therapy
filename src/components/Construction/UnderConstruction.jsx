f4cc67bae1e3157c241b20170cd9cf7b2eb4cf3c
cswebdev <csnider91@live.com>
2024-06-20 14:43:09 -0400

    updating under construction page. New background image and changed text position

---
+++ src/components/Construction/UnderConstruction.jsx
@@ -0,0 +1,32 @@
+import "./UnderConstruction.css";
+
+const UnderConstruction = () => {
+   return (
+      <>
+         <div className="Container construction-container">
+            <div className="text-container">
+               <h1 className="bg">
+                  We are making
+                  <br />
+                  some upgrades
+                  <br />
+                  to our website!
+               </h1>
+               <h2>Please check back later.</h2>
+               <p className="">
+                  {" "}
+                  Thank you, <br />
+                  Sayre Counseling
+               </p>
+               <div className="social-bar">
+                  <a href="https://www.psychologytoday.com/us/therapists/megan-sayre-libertyville-il/240981">
+                     <div className="btn btn-primary">Book an Appointment</div>
+                  </a>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+};
+
+export default UnderConstruction;
